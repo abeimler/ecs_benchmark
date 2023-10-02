668db07c4893585c59c4a69219d0d644f50ec726
abeimler <alex-beimler@web.de>
2023-10-02 21:19:43 +0200

    feat: add MonsterHeroSystems in ginseng

--- src/ginseng/ginseng/systems/MoreComplexSystem.h
+++ src/ginseng/ginseng/systems/MoreComplexSystem.h
@@ -2,11 +2,22 @@
 #define ECS_BENCHMARKS_GINSENG_MORECOMPLEXSYSTEM_H_
 
 #include "base/systems/MoreComplexSystem.h"
+#include "base/systems/HeroMonsterSystems.h"
 #include <ginseng/ginseng.hpp>
 
 namespace ecs::benchmarks::ginseng::systems {
 
 class MoreComplexSystem final : public ecs::benchmarks::base::systems::MoreComplexSystem<::ginseng::database, float> {
+public:
+  void update(EntityManager& registry, TimeDelta dt) override;
+};
+
+class HealthSystem final : public ecs::benchmarks::base::systems::HealthSystem<::ginseng::database, float> {
+public:
+  void update(EntityManager& registry, TimeDelta dt) override;
+};
+
+class DamageSystem final : public ecs::benchmarks::base::systems::DamageSystem<::ginseng::database, float> {
 public:
   void update(EntityManager& registry, TimeDelta dt) override;
 };
